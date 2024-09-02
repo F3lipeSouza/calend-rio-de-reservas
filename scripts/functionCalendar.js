576cbe36682e0bc956b3037e9d05154bf59ece1f
Felipe Souza <felipevieira67.fs@gmail.com>
2024-09-02 20:12:29 -0300

    adiciona ponto colorido abaixo do dia atual

--- scripts/functionCalendar.js
+++ scripts/functionCalendar.js
@@ -1,25 +1,46 @@
 const currentDay = new Date();
 const year = currentDay.getFullYear();
 const month = currentDay.getMonth();
+const firstDayMonth = new Date(year, month);
 const lastDayMonth = new Date(year, month + 1,0 ).getDate();
 
 const daysWeek = document.querySelector('.calendarDay');
 
-
-
+//adiciona na tela os dias do mês;
 function calendar (){
+
+    //preenche com campo vazio os primeiros dias do mês;
+    for(let dia = 1; dia <= firstDayMonth.getDay(); dia++){
+        let dias = document.createElement('p');
+        dias.innerHTML = '';
+        daysWeek.appendChild(dias);
+    }
+    
+    //preenche os dias do mês;
     for (let dia = 1; dia <= lastDayMonth; dia ++){
         let dias = document.createElement('p');
         dias.innerHTML = dia;
+        dias.className = 'day';
         daysWeek.appendChild(dias);
-        console.log(dias);
-    };
+    }
+    
+    const diadoMes = document.querySelectorAll('.day');
+    console.log(diadoMes);
+    diadoMes.forEach(day => {
+        console.log(day.textContent);
+        if(currentDay.getDate() == day.textContent){
+            const point = document.createElement('div');
+            day.appendChild(point);
+            point.id = 'today';
+            point.innerHTML = '.';
+
+        }
+    })
 }
-calendar()
 
-console.log(currentDay);
-console.log(year);
-console.log(month +1);
-console.log(lastDayMonth);
+calendar();
 
-console.log(daysWeek)
+
+
+
+
