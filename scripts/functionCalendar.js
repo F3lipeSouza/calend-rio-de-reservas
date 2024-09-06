3c2b228b8f5faa34a0ab350242ba603b61fdc8b8
Felipe Souza <felipevieira67.fs@gmail.com>
2024-09-05 21:47:43 -0300

    adiciona estilo nos dias abertos para reserva

--- scripts/functionCalendar.js
+++ scripts/functionCalendar.js
@@ -6,8 +6,59 @@ const lastDayMonth = new Date(year, month + 1,0 ).getDate();
 
 const daysWeek = document.querySelector('.calendarDay');
 
+
+const changeTitle = () =>{
+    const calendarMonth = document.querySelector('.calendar-month');
+    let calendarTitle = calendarMonth.innerHTML = '';
+    let monthName = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    calendarTitle = calendarMonth.innerHTML = `${monthName[month]}  ${year}`;
+
+}
+
+const nextMonth = () =>{
+    const next = document.querySelector('.next')//não se esqueça de fazer sua proxima funcão aqui para ,mudar o mês;
+}
+
+const putPoint = () =>{
+    //adiciona ponto colorido abaixo do dia atual;
+    const diadoMes = document.querySelectorAll('.day');
+       diadoMes.forEach(day => {
+        if(currentDay.getDate() == day.textContent){
+            day.className = 'day today';
+            const point = document.createElement('div');
+            day.appendChild(point);
+            point.id = 'today';
+            point.innerHTML = '.';
+        }
+    })
+}
+
+const openedDays = () =>{
+    let openDays = [];
+    for(let i=0; i<2; i++){
+        let aleatorio = Math.floor(Math.random()*lastDayMonth)+1;
+        openDays.push(aleatorio);
+
+        const diadoMes = document.querySelectorAll('.day');
+
+        diadoMes.forEach(day => {
+            if(openDays[0] == day.textContent){
+
+                day.className = 'openDay day';
+            };
+
+            if(openDays[1] == day.textContent){
+                day.className = 'openDay day';
+            };
+
+            
+        })
+    }
+}
+
 //adiciona na tela os dias do mês;
 function calendar (){
+    changeTitle();
 
     //preenche com campo vazio os primeiros dias do mês;
     for(let dia = 1; dia <= firstDayMonth.getDay(); dia++){
@@ -24,18 +75,9 @@ function calendar (){
         daysWeek.appendChild(dias);
     }
     
-    const diadoMes = document.querySelectorAll('.day');
-    console.log(diadoMes);
-    diadoMes.forEach(day => {
-        console.log(day.textContent);
-        if(currentDay.getDate() == day.textContent){
-            const point = document.createElement('div');
-            day.appendChild(point);
-            point.id = 'today';
-            point.innerHTML = '.';
-
-        }
-    })
+    putPoint();
+    openedDays();
+    nextMonth();
 }
 
 calendar();
